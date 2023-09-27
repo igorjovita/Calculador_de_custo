0399e3e15edee05981fb3ea3971318be5fe072b9
igorjovita <igor.jovita@hotmail.com>
2023-09-27 14:48:18 -0300

    v0.02

--- main.py
+++ main.py
@@ -2,31 +2,32 @@ import streamlit as st
 
 st.set_page_config(page_title='Calculador de Preços')
 
-with st.form('Calculo de Preços'):
-    col1, col2, col3 = st.columns(3)
 
-    with col1:
-        valor_milho = st.text_input(label='Valor do Milho', value=0)
-        valor_açucar = st.text_input(label='Valor do Açucar', value=0)
-        valor_chocolate = st.text_input(label='Valor do Chocolate', value=0)
-        valor_ninho = st.text_input(label='Valor do Leite em pó', value=0)
-        valor_local = st.text_input(label='Valor de venda para Locais',value=10)
+st.title('Amélia Doces Gourmet')
+st.subheader('Calculadora de Custos')
 
-    with col2:
-        gramatura_milho = st.text_input(label='Gramatura do Milho', value=500)
-        gramatura_açucar = st.text_input(label='Gramatura do Açucar', value=5000)
-        gramatura_chocolate = st.text_input(label='Gramatura do Chocolate', value=1010)
-        gramatura_ninho = st.text_input(label='Gramatura do Leite em pó', value=400)
-        valor_turista = st.text_input(label='Valor de venda para Turistas',value=12)
+valor_milho = st.text_input(label='Valor do Milho', value=0)
+valor_açucar = st.text_input(label='Valor do Açucar', value=0)
+valor_chocolate = st.text_input(label='Valor do Chocolate', value=0)
+valor_ninho = st.text_input(label='Valor do Leite em pó', value=0)
+valor_local = st.text_input(label='Valor de venda para Locais', value=10)
+valor_turista = st.text_input(label='Valor de venda para Turistas', value=12)
+quantidade = st.text_input(label='Rendimento da Receita', value=10)
 
 
-    with col3:
-        usado_milho = st.text_input(label='Milho na Receita', value=250)
-        usado_açucar = st.text_input(label='Açucar na Receita', value=400)
-        usado_chocolate = st.text_input(label='Chocolate na Receita', value=330)
-        usado_ninho = st.text_input(label='Leite em pó na Receita', value=200)
-        quantidade = st.text_input(label='Rendimento da Receita', value=10)
-        submit = st.form_submit_button('Calcular')
+with st.expander('Gramatura da Embalagem'):
+    gramatura_milho = st.text_input(label='Gramatura do Milho', value=500)
+    gramatura_açucar = st.text_input(label='Gramatura do Açucar', value=5000)
+    gramatura_chocolate = st.text_input(label='Gramatura do Chocolate', value=1010)
+    gramatura_ninho = st.text_input(label='Gramatura do Leite em pó', value=400)
+
+with st.expander('Gramatura da receita'):
+    usado_milho = st.text_input(label='Milho na Receita', value=250)
+    usado_açucar = st.text_input(label='Açucar na Receita', value=400)
+    usado_chocolate = st.text_input(label='Chocolate na Receita', value=330)
+    usado_ninho = st.text_input(label='Leite em pó na Receita', value=200)
+
+submit = st.button('Calcular')
 
 if submit:
     milho = (float(valor_milho) / float(gramatura_milho)) * float(usado_milho)
@@ -44,20 +45,22 @@ if submit:
     lucro_local = float(valor_local) - resultado_receita
     lucro_turista = float(valor_turista) - resultado_receita
 
-    colu1, colu2 = st.columns(2)
-    with colu1:
-        st.write(f'Valor do Milho = R$ {milho:.2f}')
-        st.write(f'Valor do Açucar = R$ {açucar:.2f}')
-        st.write(f'Valor do Chocolate = R$ {chocolate:.2f}')
-        st.write(f'Valor do Leite em Pó = R$ {ninho:.2f}')
-        st.write(f'Valor da Receita = R$ {resultado:.2f}')
-        st.write(f'Lucro venda para Local = R$ {lucro_local:.2f}')
+    st.subheader('Custo dos ingredientes na receita:')
+    st.write(f'Valor do Milho = R$ {milho:.2f}'.replace('.',','))
+    st.write(f'Valor do Açucar = R$ {açucar:.2f}'.replace('.',','))
+    st.write(f'Valor do Chocolate = R$ {chocolate:.2f}'.replace('.',','))
+    st.write(f'Valor do Leite em Pó = R$ {ninho:.2f}'.replace('.',','))
+    st.write('---')
 
+    st.subheader('Custo dos ingredientes em cada pacote:')
+    st.write(f'Custo do milho em cada pacote = R$ {milho_receita:.2f}'.replace('.', ','))
+    st.write(f'Custo do açucar em cada pacote = R$ {açucar_receita:.2f}'.replace('.', ','))
+    st.write(f'Custo do chocolate em cada pacote = R$ {chocolate_receita:.2f}'.replace('.', ','))
+    st.write(f'Custo do leite em pó em cada pacote = R$ {ninho_receita:.2f}'.replace('.', ','))
+    st.write('---')
 
-    with colu2:
-        st.write(f'Custo do milho em cada pacote = R$ {milho_receita:.2f}')
-        st.write(f'Custo do açucar em cada pacote = R$ {açucar_receita:.2f}')
-        st.write(f'Custo do chocolate em cada pacote = R$ {chocolate_receita:.2f}')
-        st.write(f'Custo do leite em pó em cada pacote = R$ {ninho_receita:.2f}')
-        st.write(f'Custo de cada pacote  = R$ {resultado_receita:.2f}')
-        st.write(f'Lucro venda para Turista = R$ {lucro_turista:.2f}')
+    st.subheader('Custo total e Lucro:')
+    st.write(f'Custo da Receita = R$ {resultado:.2f}'.replace('.', ','))
+    st.write(f'Custo de cada pacote  = R$ {resultado_receita:.2f}'.replace('.', ','))
+    st.write(f'Lucro venda para Local = R$ {lucro_local:.2f}'.replace('.', ','))
+    st.write(f'Lucro venda para Turista = R$ {lucro_turista:.2f}'.replace('.', ','))
